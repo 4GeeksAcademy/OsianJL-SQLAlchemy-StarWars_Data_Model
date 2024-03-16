a9414b56147913a0105504786f789c5752389267
Osián <149536063+OsianJL@users.noreply.github.com>
2024-03-16 11:38:48 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -7,26 +7,84 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    starships_id = Column(Integer, ForeignKey('starships.id'))
+   
+    
+    
+    
 
     def to_dict(self):
         return {}
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    email = Column(String(250), nullable=False)
+    user_favorites = relationship(Favorites)
+
+    def to_dict(self):
+        return {}
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), primary_key=True)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    characters_favorites = relationship(Favorites)
+    
+
+    def to_dict(self):
+        return {}
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    diameter = Column(Integer, nullable=False)
+    planets_favorites = relationship(Favorites)
+    
+
+    def to_dict(self):
+        return {}
+    
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    crew = Column(Integer, nullable=False)
+    passengers = Column(Integer, nullable=False)
+    consumables = Column(String(250), nullable=False)
+    cost_in_credits = Column(Integer, nullable=False)
+    starships_favorites = relationship(Favorites)
+    
+
+    def to_dict(self):
+        return {}
+    
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
+#   person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
